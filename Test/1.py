f8c985e26b70d8b9422cbff2139bb87684072dee
qwerty291996 <muxriddinisako96@gmail.com>
2025-10-12 22:13:54 +0500

    2.2 added

--- Test/1.py
+++ Test/1.py
@@ -1,1 +1,43 @@
-['2', 'Hello']
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
+import sys
+
+app = QApplication(sys.argv)
+
+win = QWidget()
+layout = QVBoxLayout(win)
+
+label = QLabel("Salom, QLabel!")
+layout.addWidget(label)
+
+
+
+
+label.setStyleSheet("""
+    font-family: 'Times New Roman';
+    font-size: 20px;
+    font-weight: bold;
+    color: darkred;
+""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+win.show()
+sys.exit(app.exec_())
