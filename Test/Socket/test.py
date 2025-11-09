272fb0e97d7b5a6547c99172b823c18a2fa54b17
qwerty291996 <muxriddinisako96@gmail.com>
2025-11-09 20:33:03 +0500

    4.1 added

--- Test/Socket/test.py
+++ Test/Socket/test.py
@@ -1,1 +1,14 @@
-print(pow(1276,893,1457))
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+# Uy ma'lumotlari (m²) va narxlari
+x = np.array([[50], [70], [100]])  # maydon
+y = np.array([40, 55, 80])         # narx (ming $)
+
+# Model yaratamiz
+model = LinearRegression()
+model.fit(x, y)
+
+# 90 m² uyning narxini taxmin qilamiz
+prediction = model.predict([[90]])
+print("Taxminiy narx:", round(prediction[0], 2), "ming $")
